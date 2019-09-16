cce2447f6e77524e3fefd9637f1ffdff90cea38e
Mika-fivem <55407199+Mika-fivem@users.noreply.github.com>
2019-09-16 21:38:45 +0200

    Update esx_rent.sql

--- esx_rent.sql
+++ esx_rent.sql
@@ -4,6 +4,7 @@ CREATE TABLE `rentable` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`store` varchar(100) NOT NULL,
 	`item` varchar(100) NOT NULL,
+	`label` varchar(100) NOT NULL,
 	`price` int(11) NOT NULL,
 
 	PRIMARY KEY (`id`)
